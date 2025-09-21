c3ef074f18e1455e12ede0c68a05ab2febedaa4d
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-09-21 17:17:09 +0530

    2 Way Data Binding - FormsModule

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  searchValue = "";
+  searchValue = "iPhone";
   changeSearchValue(eventData: any){
     // console.log((<HTMLInputElement>eventData.target).value);
     this.searchValue = (<HTMLInputElement>eventData.target).value;
