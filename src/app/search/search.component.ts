31ae54e839e9ec4bee1b0788c0dc71d23cb069e8
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-09-22 23:41:55 +0530

    *ngIf Structural Directive

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+// import { NgIf } from "../../../node_modules/@angular/common/index";
+// ✅ Correct
+import { NgIf } from '@angular/common';
+
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  searchValue = "iPhone";
+  searchValue = '';
   changeSearchValue(eventData: any){
     // console.log((<HTMLInputElement>eventData.target).value);
     this.searchValue = (<HTMLInputElement>eventData.target).value;
