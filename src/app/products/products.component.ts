c04fbeadfb81029d4f88ac0674ead9324679f07b
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-09-22 22:51:48 +0530

    [ngStyle] Directive

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,13 +10,13 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
   products = [
-    {name: "iPhone 14 Pro", price: 999, description: "Latest iPhone model with advanced features.", image : "assets/LinkedInBanner.png"},
-    {name: "Samsung Galaxy S23", price: 899, description: "Flagship Samsung phone with excellent camera.", image : "assets/LinkedInBanner.png"},
-    {name: "Google Pixel 7", price: 799, description: "Google's own phone with pure Android experience.", image : "assets/LinkedInBanner.png"},
-    {name: "OnePlus 11", price: 699, description: "High-performance phone with fast charging.", image : "assets/LinkedInBanner.png"},
-    {name: "Sony WH-1000XM5", price: 349, description: "Top-tier noise-cancelling headphones.", image : "assets/LinkedInBanner.png"},
-    {name: "Apple MacBook Pro", price: 1299, description: "Powerful laptop for professionals.", image : "assets/LinkedInBanner.png"},
-    {name: "Dell XPS 13", price: 999, description: "Compact and high-performance ultrabook.", image : "assets/LinkedInBanner.png"},
+    {name: "iPhone 14 Pro", price: 999, description: "Latest iPhone model with advanced features.", image : "assets/LinkedInBanner.png", available: "Available"},
+    {name: "Samsung Galaxy S23", price: 899, description: "Flagship Samsung phone with excellent camera.", image : "assets/LinkedInBanner.png", available : "Available"},
+    {name: "Google Pixel 7", price: 799, description: "Google's own phone with pure Android experience.", image : "assets/LinkedInBanner.png", available : "Available"},
+    {name: "OnePlus 11", price: 699, description: "High-performance phone with fast charging.", image : "assets/LinkedInBanner.png",  available : "Not Available"},
+    {name: "Sony WH-1000XM5", price: 349, description: "Top-tier noise-cancelling headphones.", image : "assets/LinkedInBanner.png",  available : "Not Available"},
+    {name: "Apple MacBook Pro", price: 1299, description: "Powerful laptop for professionals.", image : "assets/LinkedInBanner.png", available : "Available"},
+    {name: "Dell XPS 13", price: 999, description: "Compact and high-performance ultrabook.", image : "assets/LinkedInBanner.png", available : "Not Available"},
     // {name: "iPad Pro", price: 799, description: "Versatile tablet with powerful performance."},
     // {name: "Amazon Echo Dot", price: 49, description: "Smart speaker with Alexa voice assistant."},
     ]
