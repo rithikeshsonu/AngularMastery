31b4d5964730601df185f2a947ddbb82eb6b8d9b
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-09-27 18:24:55 +0530

    View Encapsulation AND <ng-content> Directive
    3 Types of ViewEncapsulation:
    ViewEncapsulation.None (Global CSS can be applied)
    ViewEncapsulation.Emulated (Angular uses this by default). Has a unique Attribute for each Component(Parent, children)
    ViewEncapsulation.ShadowDOM( You can have a CSS Globally and exclude the specific CSS by using this)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './Container/container.component';
 import { DemoNewComponent } from './demo-new/demo-new.component';
+import { ProductsNewComponent } from './products-new/products-new.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, DemoNewComponent],
+  imports: [RouterOutlet, ContainerComponent, DemoNewComponent, ProductsNewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/products-new/products-new.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-new',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-new.component.html',
+  styleUrl: './products-new.component.css'
+})
+export class ProductsNewComponent {
+
+}
